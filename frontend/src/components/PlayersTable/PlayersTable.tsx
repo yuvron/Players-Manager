12230f2dec13f4632efcc2390fc76b1f6da60fba
yuvron <yuvr99@gmail.com>
2022-08-16 15:27:11 +0300

    Create usePlayers hook and changed PlayersTable to a functional component

--- frontend/src/components/PlayersTable/PlayersTable.tsx
+++ frontend/src/components/PlayersTable/PlayersTable.tsx
@@ -1,53 +1,12 @@
-import { Component } from "react";
-import axios from "axios";
 import "./PlayersTable.scss";
 import Loader from "../Loader/Loader";
+import usePlayers from "../../hooks/usePlayers";
 
-interface Player {
-	id: number;
-	first_name: string;
-	last_name: string;
-	age: number;
-	nationality: string;
-	club: string;
-	position: string;
-	wage: number;
-	value: number;
-	clubs_history: string[];
-	agent_id: number;
-}
+const PlayersTable: React.FC = () => {
+	const players = usePlayers();
 
-interface PlayersTableState {
-	players: Player[];
-}
-
-class PlayersTable extends Component {
-	state: PlayersTableState = { players: [] };
-
-	componentDidMount() {
-		this.getPlayers();
-	}
-
-	componentDidUpdate() {
-		this.getPlayers();
-	}
-
-	getPlayers() {
-		axios
-			.get("/api/players")
-			.then((response) => {
-				if (response.data.length !== this.state.players.length) {
-					this.setState({ players: response.data });
-				}
-			})
-			.catch((err) => {
-				if (err.response && err.response.status === 500) location.reload();
-				else console.log(err);
-			});
-	}
-
-	renderPlayers() {
-		return this.state.players.map((player) => {
+	const renderPlayers = () => {
+		return players.map((player) => {
 			return (
 				<tr key={player.id}>
 					{Object.entries(player).map(([key, value]) => {
@@ -63,30 +22,28 @@ class PlayersTable extends Component {
 				</tr>
 			);
 		});
-	}
+	};
 
-	renderHeaders() {
-		if (this.state.players[0]) {
-			return Object.keys(this.state.players[0]).map((key) => {
+	const renderHeaders = () => {
+		if (players[0]) {
+			return Object.keys(players[0]).map((key) => {
 				const formattedKey = key.replaceAll("_", " ");
 				return <th key={key}>{formattedKey}</th>;
 			});
 		} else return [];
-	}
+	};
 
-	render() {
-		if (this.state.players.length === 0) return <Loader />;
-		return (
-			<div className="users-table">
-				<table>
-					<thead>
-						<tr>{this.renderHeaders()}</tr>
-					</thead>
-					<tbody>{this.renderPlayers()}</tbody>
-				</table>
-			</div>
-		);
-	}
-}
+	if (players.length === 0) return <Loader />;
+	return (
+		<div className="users-table">
+			<table>
+				<thead>
+					<tr>{renderHeaders()}</tr>
+				</thead>
+				<tbody>{renderPlayers()}</tbody>
+			</table>
+		</div>
+	);
+};
 
 export default PlayersTable;
---
+++ frontend/src/hooks/usePlayers.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Player {
+	id: number;
+	first_name: string;
+	last_name: string;
+	age: number;
+	nationality: string;
+	club: string;
+	position: string;
+	wage: number;
+	value: number;
+	clubs_history: string[];
+	agent_id: number;
+}
+
+const usePlayers = () => {
+	const [players, setPlayers] = useState<Player[]>([]);
+
+	useEffect(() => {
+		getPlayers();
+	});
+
+	const getPlayers = () => {
+		axios
+			.get("/api/players")
+			.then((response) => {
+				if (response.data.length !== players.length) {
+					setPlayers(response.data);
+				}
+			})
+			.catch((err) => {
+				if (err.response && err.response.status === 500) location.reload();
+				else console.log(err);
+			});
+	};
+
+	return players;
+};
+
+export default usePlayers;
